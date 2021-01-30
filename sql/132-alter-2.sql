1d40122176e5cb5e96bb58fd306fdf8d09e5d2db
Fahid Khan <fahid.khan@tuni.fi>
2021-01-31 00:09:44 +0200

    Final Solution

--- sql/132-alter-2.sql
+++ sql/132-alter-2.sql
@@ -11,10 +11,3 @@ SET      address = CASE id WHEN 1 THEN '1st street, New York, NY'
 					  WHERE id IN(1,3,4)
 
 
-UPDATE members
-SET      address = CASE id WHEN 1 THEN '1st street, New York, NY'
-                      WHEN 3 THEN 'Brooklyn Bridge cross 2b, New York, NY'
-					  WHEN 4 THEN 'Queens palisander avenue 3, New York, NY'
-					  
-					  END
-					  WHERE id IN(1,3,4)
